7b7fdbdd529c48c706d50550b2eac74dbb0a65ce
ANGEL <angel.lr97@gmail.com>
2020-05-29 20:43:15 +0200

    29 May 20:43

---
+++ main.py
@@ -0,0 +1,13 @@
+import cv2 as cv
+import numpy as np
+import os
+from sklearn.cluster import KMeans
+from scipy.spatial import distance
+
+def main():
+    """
+    Main class of the program.
+    """ 
+
+if __name__ == "__main__":
+    main()
